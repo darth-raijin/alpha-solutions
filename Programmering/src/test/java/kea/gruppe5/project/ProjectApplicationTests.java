296e5a6db02616ded4e26b05e2228d79571f74ec
macow-lab <mmacovv@gmail.com>
2021-11-22 10:18:13 +0100

    Initial

---
+++ Programmering/src/test/java/kea/gruppe5/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.gruppe5.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
